290fb6cf52989b9e5e2ae177652d7d090dcf9508
stuge <tuukkaveteli@gmail.com>
2021-02-22 23:44:05 +0200

    heroku

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
 let PORT = process.env.PORT
 let MONGOURL = process.env.MONGOURL
-
+ console.log('process.env :>> ', process.env);
 if (process.env.NODE_ENV === 'test') {
     MONGOURL = process.env.TEST_MONGOURL
 }
