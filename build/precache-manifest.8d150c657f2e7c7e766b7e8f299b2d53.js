9f95d79c7d962e89acd2798669ccce0d143254d7
stuge <tuukkaveteli@gmail.com>
2021-02-22 23:07:05 +0200

    aa

---
+++ build/precache-manifest.8d150c657f2e7c7e766b7e8f299b2d53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091074b674c5ca29bbedddf2bbc8e591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4546ad51d9015350e820",
+    "url": "/static/css/main.fdd00c7b.chunk.css"
+  },
+  {
+    "revision": "e3249b2c9f9d367e9e05",
+    "url": "/static/js/2.64fa2067.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.64fa2067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4546ad51d9015350e820",
+    "url": "/static/js/main.4cd0a187.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
