d7d9dc1cc98f50371ac41b0de3462e686f3903ae
stuge <tuukkaveteli@gmail.com>
2021-03-01 15:37:06 +0200

    trying to fix the refresh issue

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -22,6 +22,7 @@ blogRouter.post('/', async (request, response) => {
     if (!request.token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
     }
+    console.log('decodedToken :>> ', decodedToken);
     const user = await User.findById(decodedToken.id)
     const blog = new Blog({
         title: body.title,
--- controllers/login.js
+++ controllers/login.js
@@ -6,7 +6,6 @@ const User = require('../models/user')
 loginRouter.post('/', async (req, res) => {
     const body = req.body
     const user = await User.findOne({ username: body.username })
-    console.log('user :>> ', user);
     console.log(await bcrypt.compare(body.password, user.passwordHash));
     const passwordCorrect = user === null
         ? false
