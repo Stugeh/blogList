21738259453f2c898a48012eb6c05db42192eb8c
stuge <tuukkaveteli@gmail.com>
2021-07-23 14:25:45 +0300

    trying to make webpack do its fucking job

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 import '../public/favicon.ico'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 ReactDOM.render(<App />, document.getElementById('root'))
